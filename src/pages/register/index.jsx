2e40effc0b6c4568e250facfa1f657ba33d2d875
luka mkalavishvili <mkalavishvililuka00@gmail.com>
2023-03-09 16:20:27 +0400

    Tailwind fix

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -8,7 +8,7 @@ import Label from '@/components/Label'
 import Link from 'next/link'
 import { useAuth } from '@/hooks/auth'
 import { useState, useRef } from 'react'
-import 'tailwindcss/tailwind.css'
+import Head from 'next/head'
 
 const Register = () => {
     const { register } = useAuth({
@@ -40,102 +40,108 @@ const Register = () => {
     }
 
     return (
-        <GuestLayout>
-            <AuthCard
-                logo={
-                    <Link href="/">
-                        <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
-                    </Link>
-                }>
-                <form ref={form} onSubmit={submitForm}>
-                    {/* Name */}
-                    <div>
-                        <Label htmlFor="name">Name</Label>
+        <>
+            <Head>
+                <script src="https://cdn.tailwindcss.com"></script>
 
-                        <Input
-                            id="name"
-                            type="text"
-                            value={name}
-                            className="block mt-1 w-full"
-                            onChange={event => setName(event.target.value)}
-                            required
-                            autoFocus
-                        />
-
-                        <InputError messages={errors.name} className="mt-2" />
-                    </div>
-
-                    {/* Email Address */}
-                    <div className="mt-4">
-                        <Label htmlFor="email">Email</Label>
-
-                        <Input
-                            id="email"
-                            type="email"
-                            value={email}
-                            className="block mt-1 w-full"
-                            onChange={event => setEmail(event.target.value)}
-                            required
-                        />
-
-                        <InputError messages={errors.email} className="mt-2" />
-                    </div>
-
-                    {/* Password */}
-                    <div className="mt-4">
-                        <Label htmlFor="password">Password</Label>
-
-                        <Input
-                            id="password"
-                            type="password"
-                            value={password}
-                            className="block mt-1 w-full"
-                            onChange={event => setPassword(event.target.value)}
-                            required
-                            autoComplete="new-password"
-                        />
-
-                        <InputError
-                            messages={errors.password}
-                            className="mt-2"
-                        />
-                    </div>
-
-                    {/* Confirm Password */}
-                    <div className="mt-4">
-                        <Label htmlFor="passwordConfirmation">
-                            Confirm Password
-                        </Label>
-
-                        <Input
-                            id="passwordConfirmation"
-                            type="password"
-                            value={passwordConfirmation}
-                            className="block mt-1 w-full"
-                            onChange={event =>
-                                setPasswordConfirmation(event.target.value)
-                            }
-                            required
-                        />
-
-                        <InputError
-                            messages={errors.password_confirmation}
-                            className="mt-2"
-                        />
-                    </div>
-
-                    <div className="flex items-center justify-end mt-4">
-                        <Link
-                            href="/login"
-                            className="underline text-sm text-gray-600 hover:text-gray-900">
-                            Already registered?
+            </Head>
+            <GuestLayout>
+                <AuthCard
+                    logo={
+                        <Link href="/">
+                            <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
                         </Link>
+                    }>
+                    <form ref={form} onSubmit={submitForm}>
+                        {/* Name */}
+                        <div>
+                            <Label htmlFor="name">Name</Label>
 
-                        <Button className="ml-4">Register</Button>
-                    </div>
-                </form>
-            </AuthCard>
-        </GuestLayout>
+                            <Input
+                                id="name"
+                                type="text"
+                                value={name}
+                                className="block mt-1 w-full"
+                                onChange={event => setName(event.target.value)}
+                                required
+                                autoFocus
+                            />
+
+                            <InputError messages={errors.name} className="mt-2" />
+                        </div>
+
+                        {/* Email Address */}
+                        <div className="mt-4">
+                            <Label htmlFor="email">Email</Label>
+
+                            <Input
+                                id="email"
+                                type="email"
+                                value={email}
+                                className="block mt-1 w-full"
+                                onChange={event => setEmail(event.target.value)}
+                                required
+                            />
+
+                            <InputError messages={errors.email} className="mt-2" />
+                        </div>
+
+                        {/* Password */}
+                        <div className="mt-4">
+                            <Label htmlFor="password">Password</Label>
+
+                            <Input
+                                id="password"
+                                type="password"
+                                value={password}
+                                className="block mt-1 w-full"
+                                onChange={event => setPassword(event.target.value)}
+                                required
+                                autoComplete="new-password"
+                            />
+
+                            <InputError
+                                messages={errors.password}
+                                className="mt-2"
+                            />
+                        </div>
+
+                        {/* Confirm Password */}
+                        <div className="mt-4">
+                            <Label htmlFor="passwordConfirmation">
+                                Confirm Password
+                            </Label>
+
+                            <Input
+                                id="passwordConfirmation"
+                                type="password"
+                                value={passwordConfirmation}
+                                className="block mt-1 w-full"
+                                onChange={event =>
+                                    setPasswordConfirmation(event.target.value)
+                                }
+                                required
+                            />
+
+                            <InputError
+                                messages={errors.password_confirmation}
+                                className="mt-2"
+                            />
+                        </div>
+
+                        <div className="flex items-center justify-end mt-4">
+                            <Link
+                                href="/login"
+                                className="underline text-sm text-gray-600 hover:text-gray-900">
+                                Already registered?
+                            </Link>
+
+                            <Button className="ml-4">Register</Button>
+                        </div>
+                    </form>
+                </AuthCard>
+            </GuestLayout>
+        </>
     )
 }
 
