c8717ed6f654eb93acb6817ab95426c5330646b2
luka mkalavishvili <mkalavishvililuka00@gmail.com>
2023-03-13 13:49:18 +0400

    forgot password flow

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,4 +1,8 @@
+import Link from "next/link"
+
 const NotFoundPage = () => (
+
+
     <div className="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
         <div className="max-w-xl mx-auto sm:px-6 lg:px-8">
             <div className="flex items-center pt-8 sm:justify-start sm:pt-0">
@@ -7,7 +11,7 @@ const NotFoundPage = () => (
                 </div>
 
                 <div className="ml-4 text-lg text-gray-500 uppercase tracking-wider">
-                    Not Found
+                    <Link href="/   ">Return to hompage</Link>
                 </div>
             </div>
         </div>
