839015af65758b0b7de9d268456bed544d37a2ae
nuguntu <nskhiereli@gmail.com>
2023-04-11 17:04:49 +0400

    chore

--- src/pages/faq/index.jsx
+++ src/pages/faq/index.jsx
@@ -1,50 +1,56 @@
-import Header from '@/pages/header';
-import Footer from '@/pages/footer';
-import Head from 'next/head';
+import Header from '@/pages/header'
+import Footer from '@/pages/footer'
+import Head from 'next/head'
 import styles from '@/styles/faq/faq.module.css'
-import { useRef } from 'react';
-import Link from 'next/link';
+import { useRef } from 'react'
+import Link from 'next/link'
 
-export default function Faq({isLogged, user, logout, search, data}) {
+export default function Faq({ isLogged, user, logout, search, data }) {
     const parent = useRef()
     const form = useRef()
     const input = useRef()
 
     let faqData = {
-      "Category 1":[
-        {
-          heading: "Question? 1",
-          content: "It is agreed that the client wishes to market a new platform allowing recruiters and candidates in the restaurant and hotel sector to register and offer their job offers online or to apply for a job offer. Thus, it will be possible for candidates to search on the platform for a specific job offer according to pre-defined criteria and to apply for it."
-        },
-        {
-          heading: "Question? 2",
-          content: "It is agreed that the client wishes to market a new platform allowing recruiters and candidates in the restaurant and hotel sector to register and offer their job offers online or to apply for a job offer. Thus, it will be possible for candidates to search on the platform for a specific job offer according to pre-defined criteria and to apply for it."
-        },
-        {
-          heading: "Question? 3",
-          content: "It is agreed that the client wishes to market a new platform allowing recruiters and candidates in the restaurant and hotel sector to register and offer their job offers online or to apply for a job offer. Thus, it will be possible for candidates to search on the platform for a specific job offer according to pre-defined criteria and to apply for it."
-        }
-      ],
-  
-      "Category 2":[
-        
-        {
-            heading: "Question? 4",
-            content: "It is agreed that the client wishes to market a new platform allowing recruiters and candidates in the restaurant and hotel sector to register and offer their job offers online or to apply for a job offer. Thus, it will be possible for candidates to search on the platform for a specific job offer according to pre-defined criteria and to apply for it."
-        },
-        {
-          heading: "Question? 5",
-          content: "It is agreed that the client wishes to market a new platform allowing recruiters and candidates in the restaurant and hotel sector to register and offer their job offers online or to apply for a job offer. Thus, it will be possible for candidates to search on the platform for a specific job offer according to pre-defined criteria and to apply for it."
-        },
-        {
-          heading: "Question? 6",
-          content: "It is agreed that the client wishes to market a new platform allowing recruiters and candidates in the restaurant and hotel sector to register and offer their job offers online or to apply for a job offer. Thus, it will be possible for candidates to search on the platform for a specific job offer according to pre-defined criteria and to apply for it."
-        },
-        {
-          heading: "Question? 7",
-          content: "It is agreed that the client wishes to market a new platform allowing recruiters and candidates in the restaurant and hotel sector to register and offer their job offers online or to apply for a job offer. Thus, it will be possible for candidates to search on the platform for a specific job offer according to pre-defined criteria and to apply for it."
-        }
-      ]
+        'Category 1': [
+            {
+                heading: 'Question? 1',
+                content:
+                    'It is agreed that the client wishes to market a new platform allowing recruiters and candidates in the restaurant and hotel sector to register and offer their job offers online or to apply for a job offer. Thus, it will be possible for candidates to search on the platform for a specific job offer according to pre-defined criteria and to apply for it.',
+            },
+            {
+                heading: 'Question? 2',
+                content:
+                    'It is agreed that the client wishes to market a new platform allowing recruiters and candidates in the restaurant and hotel sector to register and offer their job offers online or to apply for a job offer. Thus, it will be possible for candidates to search on the platform for a specific job offer according to pre-defined criteria and to apply for it.',
+            },
+            {
+                heading: 'Question? 3',
+                content:
+                    'It is agreed that the client wishes to market a new platform allowing recruiters and candidates in the restaurant and hotel sector to register and offer their job offers online or to apply for a job offer. Thus, it will be possible for candidates to search on the platform for a specific job offer according to pre-defined criteria and to apply for it.',
+            },
+        ],
+
+        'Category 2': [
+            {
+                heading: 'Question? 4',
+                content:
+                    'It is agreed that the client wishes to market a new platform allowing recruiters and candidates in the restaurant and hotel sector to register and offer their job offers online or to apply for a job offer. Thus, it will be possible for candidates to search on the platform for a specific job offer according to pre-defined criteria and to apply for it.',
+            },
+            {
+                heading: 'Question? 5',
+                content:
+                    'It is agreed that the client wishes to market a new platform allowing recruiters and candidates in the restaurant and hotel sector to register and offer their job offers online or to apply for a job offer. Thus, it will be possible for candidates to search on the platform for a specific job offer according to pre-defined criteria and to apply for it.',
+            },
+            {
+                heading: 'Question? 6',
+                content:
+                    'It is agreed that the client wishes to market a new platform allowing recruiters and candidates in the restaurant and hotel sector to register and offer their job offers online or to apply for a job offer. Thus, it will be possible for candidates to search on the platform for a specific job offer according to pre-defined criteria and to apply for it.',
+            },
+            {
+                heading: 'Question? 7',
+                content:
+                    'It is agreed that the client wishes to market a new platform allowing recruiters and candidates in the restaurant and hotel sector to register and offer their job offers online or to apply for a job offer. Thus, it will be possible for candidates to search on the platform for a specific job offer according to pre-defined criteria and to apply for it.',
+            },
+        ],
     }
 
     // faqData = data.faqs
@@ -55,23 +61,29 @@ export default function Faq({isLogged, user, logout, search, data}) {
     let searchedDataCategories = []
     let searchedData = []
 
-    const toggleAccordion = (mainIndex, index)=>{
-        document.querySelectorAll('.expandAccordionParent')[mainIndex]
-        .querySelectorAll('.expandAccordion')[index]
-        .classList.toggle('openAccordion')
+    const toggleAccordion = (mainIndex, index) => {
+        document
+            .querySelectorAll('.expandAccordionParent')
+            [mainIndex].querySelectorAll('.expandAccordion')
+            [index].classList.toggle('openAccordion')
     }
 
-    function submitForm(e){
+    function submitForm(e) {
         e.preventDefault()
-        if(input.current.value.length > 0) form.current.submit()
+        if (input.current.value.length > 0) form.current.submit()
     }
-    
-    if(search) {
-        faqKeys.forEach((key, keyIndex) =>{
+
+    if (search) {
+        faqKeys.forEach((key, keyIndex) => {
             let newArr = []
-            faqValues[keyIndex].forEach(value =>{
-                if(value.heading.toLowerCase().includes(search.toLowerCase()) || value.content.toLowerCase().includes(search.toLowerCase())) {
-                    if(!searchedDataCategories.includes(key)) {
+            faqValues[keyIndex].forEach(value => {
+                if (
+                    value.heading
+                        .toLowerCase()
+                        .includes(search.toLowerCase()) ||
+                    value.content.toLowerCase().includes(search.toLowerCase())
+                ) {
+                    if (!searchedDataCategories.includes(key)) {
                         searchedDataCategories.push(key)
                     }
                     newArr.push(value)
@@ -83,17 +95,24 @@ export default function Faq({isLogged, user, logout, search, data}) {
 
     let displayData
 
-
-
-    if(!search){
+    if (!search) {
         displayData = faqKeys.map((item, mainIndex) => {
-            return(
-                <div key={mainIndex} className={`expandAccordionParent ${styles.accordionParent}`}>
+            return (
+                <div
+                    key={mainIndex}
+                    className={`expandAccordionParent ${styles.accordionParent}`}>
                     <h1>{item}</h1>
-                    {faqValues[mainIndex].map((item, index) =>{
-                        return(
-                            <div key={index} className={`expandAccordion ${styles.accordion}`} ref={parent} >
-                                <h1 className={styles.header} onClick={()=> toggleAccordion(mainIndex, index)}>
+                    {faqValues[mainIndex].map((item, index) => {
+                        return (
+                            <div
+                                key={index}
+                                className={`expandAccordion ${styles.accordion}`}
+                                ref={parent}>
+                                <h1
+                                    className={styles.header}
+                                    onClick={() =>
+                                        toggleAccordion(mainIndex, index)
+                                    }>
                                     {item.question}
                                 </h1>
                                 <div className={styles.content}>
@@ -105,15 +124,24 @@ export default function Faq({isLogged, user, logout, search, data}) {
                 </div>
             )
         })
-    }else{
+    } else {
         displayData = searchedDataCategories.map((item, mainIndex) => {
-            return(
-                <div key={mainIndex} className={`expandAccordionParent ${styles.accordionParent}`}>
+            return (
+                <div
+                    key={mainIndex}
+                    className={`expandAccordionParent ${styles.accordionParent}`}>
                     <h1>{item}</h1>
-                    {searchedData[mainIndex].map((item, index) =>{
-                        return(
-                            <div key={index} className={`expandAccordion ${styles.accordion}`} ref={parent} >
-                                <h1 className={styles.header} onClick={()=> toggleAccordion(mainIndex, index)}>
+                    {searchedData[mainIndex].map((item, index) => {
+                        return (
+                            <div
+                                key={index}
+                                className={`expandAccordion ${styles.accordion}`}
+                                ref={parent}>
+                                <h1
+                                    className={styles.header}
+                                    onClick={() =>
+                                        toggleAccordion(mainIndex, index)
+                                    }>
                                     {item.heading}
                                 </h1>
                                 <div className={styles.content}>
@@ -127,27 +155,45 @@ export default function Faq({isLogged, user, logout, search, data}) {
         })
     }
 
-    if(displayData.length === 0){
-        return(
+    if (displayData.length === 0) {
+        return (
             <>
                 <Head>
                     <title>F.A.Q</title>
                 </Head>
-    
-                <Header isLogged={isLogged} user={user} active="faq"/>
-    
+
+                <Header isLogged={isLogged} user={user} active="faq" />
+
                 <main className={styles.main}>
                     <h1 className={styles.mainHeader}>Foire Aux Questions</h1>
-                    <h2 className={styles.intro}>Avez-vous des questions ? Nous sommes là pour vous répondre.</h2>
+                    <h2 className={styles.intro}>
+                        Avez-vous des questions ? Nous sommes là pour vous
+                        répondre.
+                    </h2>
                     <form action="" className={styles.faqForm} ref={form}>
-                        <i className={`fa-solid fa-magnifying-glass ${styles.glass}`}></i>
-                        <input type="text" name="search" placeholder="Recherche" required ref={input} defaultValue={search != false ? search : ''}/>
-                        <i className={`fa-solid fa-arrow-right ${styles.arrowRight}`} onClick={(e)=>{submitForm(e)}}></i>
+                        <i
+                            className={`fa-solid fa-magnifying-glass ${styles.glass}`}></i>
+                        <input
+                            type="text"
+                            name="search"
+                            placeholder="Recherche"
+                            required
+                            ref={input}
+                            defaultValue={search != false ? search : ''}
+                        />
+                        <i
+                            className={`fa-solid fa-arrow-right ${styles.arrowRight}`}
+                            onClick={e => {
+                                submitForm(e)
+                            }}></i>
                     </form>
-    
+
                     <div className={styles.noResults}>
                         <img src="/no-results-character.png" alt="" />
-                        <h3>Désolé, nous n'avons pas trouvé de résultats correspondant à <br/>“{search}”.</h3>
+                        <h3>
+                            Désolé, nous n'avons pas trouvé de résultats
+                            correspondant à <br />“{search}”.
+                        </h3>
                     </div>
 
                     <div className={styles.searchTips}>
@@ -162,16 +208,18 @@ export default function Faq({isLogged, user, logout, search, data}) {
 
                     <div className={styles.sendRequest}>
                         <h3>Vous souhaitez nous contacter ?</h3>
-                        <p>Envoyez-nous votre question et notre équipe vous reviendra dans les plus brefs délais.</p>
+                        <p>
+                            Envoyez-nous votre question et notre équipe vous
+                            reviendra dans les plus brefs délais.
+                        </p>
                         <Link href="/about">CONTACTEZ-NOUS</Link>
                     </div>
                 </main>
-    
+
                 <Footer />
             </>
         )
     }
-
 
     return (
         <>
@@ -179,51 +227,66 @@ export default function Faq({isLogged, user, logout, search, data}) {
                 <title>F.A.Q</title>
             </Head>
 
-            <Header isLogged={isLogged} user={user} active="faq"/>
+            <Header isLogged={isLogged} user={user} active="faq" />
 
             <main className={styles.main}>
-                <h1 className={styles.mainHeader}>Frequently Asked Questions</h1>
-                <h2 className={styles.intro}>Do you have questions? We’re here to help.</h2>
+                <h1 className={styles.mainHeader}>
+                    Frequently Asked Questions
+                </h1>
+                <h2 className={styles.intro}>
+                    Do you have questions? We’re here to help.
+                </h2>
                 <form action="" className={styles.faqForm} ref={form}>
-                    <i className={`fa-solid fa-magnifying-glass ${styles.glass}`}></i>
-                    <input type="text" name="search" placeholder="Rechercher" required ref={input} defaultValue={search != false ? search : ''}/>
-                    <i className={`fa-solid fa-arrow-right ${styles.arrowRight}`} onClick={(e)=>{submitForm(e)}}></i>
+                    <i
+                        className={`fa-solid fa-magnifying-glass ${styles.glass}`}></i>
+                    <input
+                        type="text"
+                        name="search"
+                        placeholder="Rechercher"
+                        required
+                        ref={input}
+                        defaultValue={search != false ? search : ''}
+                    />
+                    <i
+                        className={`fa-solid fa-arrow-right ${styles.arrowRight}`}
+                        onClick={e => {
+                            submitForm(e)
+                        }}></i>
                 </form>
 
-                <div className={styles.accordionContainer}>
-                    {
-                        displayData
-                    }
-                </div>
+                <div className={styles.accordionContainer}>{displayData}</div>
             </main>
 
             <Footer />
         </>
-    );
+    )
 }
 
-export async function getServerSideProps(context){
-
+export async function getServerSideProps(context) {
     let data
 
-    if(context.query.search){
-        console.log(context.query.search);
-        // const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/faq`)
-        // data = await response.json()
-        return{
-            props: {
-                search: context.query.search
-            }
-        }
-    }else{
-        const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/faq`)
+    if (context.query.search) {
+        console.log(context.query.search)
+        const response = await fetch(
+            `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/faq`,
+        )
         data = await response.json()
-        return{
+        console.log(data)
+        return {
+            props: {
+                search: context.query.search,
+            },
+        }
+    } else {
+        const response = await fetch(
+            `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/faq`,
+        )
+        data = await response.json()
+        return {
             props: {
                 search: false,
-                data
-            }
+                data,
+            },
         }
     }
-
 }
--- src/pages/jobs/index.jsx
+++ src/pages/jobs/index.jsx
@@ -1,44 +1,47 @@
-import Banner from '@/components/jobsComponents/Banner';
+import Banner from '@/components/jobsComponents/Banner'
 import JobList from '@/components/jobsComponents/JobList'
-import GeneralInformation from '@/components/jobsComponents/generalInformation'
-import Header from '@/pages/header';
-import Footer from '@/pages/footer';
-import Head from 'next/head';
-import TipsList from '@/components/TipsList';
-import CountryJobs from '@/components/jobsComponents/CountryJobs';
-import {countries} from '@/components/countries';
+import GeneralInformation from '@/components/jobsComponents/GeneralInformation'
+import Header from '@/pages/header'
+import Footer from '@/pages/footer'
+import Head from 'next/head'
+import TipsList from '@/components/TipsList'
+import CountryJobs from '@/components/jobsComponents/CountryJobs'
+import { countries } from '@/components/countries'
 import styles from '@/styles/homepage/homepage.module.css'
 
-export default function Jobs({jobDataList, isLogged, user, logout}, props) {
-
+export default function Jobs({ jobDataList, isLogged, user, logout }, props) {
     let tipsData = {
-        "Category 1":[
-          {
-            id: 1,
-            date: "13.03.2023",
-            heading: "What is the platform for?",
-            content: "It is agreed that the client wishes to market a new platform allowing recruiters and candidates in the restaurant and hotel sector to register and offer their job offers online or to apply for a job offer. Thus, it will be possible for candidates to search on the platform for a specific job offer according to pre-defined criteria and to apply for it."
-          },
-          {
-            id: 2,
-            date: "13.03.2023",
-            heading: "What is the platform for?",
-            content: "It is agreed that the client wishes to market a new platform allowing recruiters and candidates in the restaurant and hotel sector to register and offer their job offers online or to apply for a job offer. Thus, it will be possible for candidates to search on the platform for a specific job offer according to pre-defined criteria and to apply for it."
-          },
-          {
-            id: 3,
-            date: "13.03.2023",
-            heading: "What is the platform for?",
-            content: "It is agreed that the client wishes to market a new platform allowing recruiters and candidates in the restaurant and hotel sector to register and offer their job offers online or to apply for a job offer. Thus, it will be possible for candidates to search on the platform for a specific job offer according to pre-defined criteria and to apply for it."
-          },
-          {
-            id: 4,
-            date: "13.03.2023",
-            heading: "What is the platform for?",
-            content: "It is agreed that the client wishes to market a new platform allowing recruiters and candidates in the restaurant and hotel sector to register and offer their job offers online or to apply for a job offer. Thus, it will be possible for candidates to search on the platform for a specific job offer according to pre-defined criteria and to apply for it."
-          },
+        'Category 1': [
+            {
+                id: 1,
+                date: '13.03.2023',
+                heading: 'What is the platform for?',
+                content:
+                    'It is agreed that the client wishes to market a new platform allowing recruiters and candidates in the restaurant and hotel sector to register and offer their job offers online or to apply for a job offer. Thus, it will be possible for candidates to search on the platform for a specific job offer according to pre-defined criteria and to apply for it.',
+            },
+            {
+                id: 2,
+                date: '13.03.2023',
+                heading: 'What is the platform for?',
+                content:
+                    'It is agreed that the client wishes to market a new platform allowing recruiters and candidates in the restaurant and hotel sector to register and offer their job offers online or to apply for a job offer. Thus, it will be possible for candidates to search on the platform for a specific job offer according to pre-defined criteria and to apply for it.',
+            },
+            {
+                id: 3,
+                date: '13.03.2023',
+                heading: 'What is the platform for?',
+                content:
+                    'It is agreed that the client wishes to market a new platform allowing recruiters and candidates in the restaurant and hotel sector to register and offer their job offers online or to apply for a job offer. Thus, it will be possible for candidates to search on the platform for a specific job offer according to pre-defined criteria and to apply for it.',
+            },
+            {
+                id: 4,
+                date: '13.03.2023',
+                heading: 'What is the platform for?',
+                content:
+                    'It is agreed that the client wishes to market a new platform allowing recruiters and candidates in the restaurant and hotel sector to register and offer their job offers online or to apply for a job offer. Thus, it will be possible for candidates to search on the platform for a specific job offer according to pre-defined criteria and to apply for it.',
+            },
         ],
-      }
+    }
 
     return (
         <>
@@ -46,47 +49,53 @@ export default function Jobs({jobDataList, isLogged, user, logout}, props) {
                 <title>Emplois</title>
             </Head>
 
-            <Header isLogged={isLogged} user={user} active="job"/>
+            <Header isLogged={isLogged} user={user} active="job" />
 
             <Banner />
-              
+
             <h1 className={styles.jobListHeading}>Trouver un emploi</h1>
 
-            <JobList data={jobDataList}/>
+            <JobList data={jobDataList} />
 
             <GeneralInformation />
 
-            <TipsList data={tipsData} isHeader={true}/>
-            
-            <CountryJobs data={countries}/>
+            <TipsList data={tipsData} isHeader={true} />
 
-            <Footer className="noMargin"/>
+            <CountryJobs data={countries} />
+
+            <Footer className="noMargin" />
         </>
-    );
+    )
 }
 
-export async function getServerSideProps(context){
-    let data;
+export async function getServerSideProps(context) {
+    let data
 
-    if(context.resolvedUrl.includes('/jobs?search=')){
-        const response = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL+'/api/v1'+context.resolvedUrl)
+    if (context.resolvedUrl.includes('/jobs?search=')) {
+        const response = await fetch(
+            process.env.NEXT_PUBLIC_BACKEND_URL +
+                '/api/v1' +
+                context.resolvedUrl,
+        )
         data = await response.json()
-    }else{
-        const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/jobs`)
+    } else {
+        const response = await fetch(
+            `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/jobs`,
+        )
         data = await response.json()
     }
 
-    if(data.data){
-      return{
-          props: {
-              jobDataList: data.data,
-          }
-      }
-    }else{
-      return{
-          props: {
-              jobDataList: data,
-          }
-      }
+    if (data.data) {
+        return {
+            props: {
+                jobDataList: data.data,
+            },
+        }
+    } else {
+        return {
+            props: {
+                jobDataList: data,
+            },
+        }
     }
 }
